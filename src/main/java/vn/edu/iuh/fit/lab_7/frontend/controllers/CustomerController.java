685815aa7bcdf38080e7aae926d345a9d159c09a
Student <anhchung546@gmail.com>
2023-11-04 14:48:39 +0700

    reponsitory_service_controller

--- src/main/java/vn/edu/iuh/fit/lab_7/frontend/controllers/CustomerController.java
+++ src/main/java/vn/edu/iuh/fit/lab_7/frontend/controllers/CustomerController.java
@@ -5,6 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import vn.edu.iuh.fit.lab_7.backend.models.Customer;
 import vn.edu.iuh.fit.lab_7.backend.repositories.CustomerRepository;
@@ -16,30 +19,30 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Controller
+@RequestMapping("/customer")
 public class CustomerController {
     @Autowired
     private CustomerService service;
     @Autowired
     private CustomerRepository repository;
 
-    @GetMapping("/customer")
+    @GetMapping("/list")
     public String showCustomerListPaging(Model model,
                                          @RequestParam("page")Optional<Integer> page,
                                          @RequestParam("size")Optional<Integer> size){
     int currentPage = page.orElse(1);
     int pageSize = size.orElse(10);
-        Page<Customer> customerPage = service.findPaginated(currentPage - 1, pageSize,"name","asc");
-        model.addAttribute("customerPage",customerPage);
-        int totalPages = customerPage.getTotalPages();
-        if(totalPages>0){
+    Page<Customer> customerPage = service.findPaginated(currentPage - 1, pageSize,"name","asc");
+    model.addAttribute("customerPage",customerPage);
+    int totalPages = customerPage.getTotalPages();
+    if(totalPages>0){
             List<Integer> pageNumbers = IntStream.rangeClosed(1,totalPages)
                     .boxed()
                     .collect(Collectors.toList());
             model.addAttribute("pageNumbers",pageNumbers);
 
-        }
-
-        return "admin/customer/listing";
+    }
+    return "admin/customer/listing";
 
     }
 }
