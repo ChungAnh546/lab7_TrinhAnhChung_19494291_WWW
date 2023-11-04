15288f415d610e8e6531a2314080c5d197555a5b
Student <anhchung546@gmail.com>
2023-11-04 13:27:48 +0700

    create

---
+++ src/main/java/vn/edu/iuh/fit/lab_7/LabTrinhAnhChung19494291WwwApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.lab_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabTrinhAnhChung19494291WwwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabTrinhAnhChung19494291WwwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/lab_7/LabTrinhAnhChung19494291WwwApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.lab_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabTrinhAnhChung19494291WwwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
